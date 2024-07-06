83db9d904626a04b0c936b1d36ede7f6ac40c119
Ramazan Kucukkoc <ramazankckkc4234@gmail.com>
2024-07-06 17:14:47 +0300

    Add project files.

---
+++ SoapTestCreate/ICalculator.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceModel;
+
+[ServiceContract]
+public interface ICalculator
+{
+    [OperationContract]
+    string Add(int a, int b);
+}
+
+public class CalculatorService : ICalculator
+{
+    public string Add(int a, int b)
+    {
+        return "Ramazan Kucukkoc";
+    }
+}
